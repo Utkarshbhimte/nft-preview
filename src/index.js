47479af3174f8e3e6e2e1521af92fce1f7e5823e
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2021-10-15 03:40:36 +0530

    tailwind fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.render(
   <Router>
